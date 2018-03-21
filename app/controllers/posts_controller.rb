be59b2c2580e84a10297594efc99a2028cd1cdc0
Iann Singer <itpsinger@gmail.com>
2018-03-21 20:16:06 +0000

    Create post model and associations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,14 +1,17 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def show
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.create
   end
-  
+
 end

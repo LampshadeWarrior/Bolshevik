deb038f29c60fe802242a2eba0de75783503987f
Iann Singer <itpsinger@gmail.com>
2018-03-26 15:57:15 +0000

    Add comment controller and model and set up routes

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,18 @@
+class CommentsController < ApplicationController
+before_action :authenticate_user!
+  def new
+    @comment = Comment.new
+  end
+
+  def create
+    @post = Post.find(params[:format])
+    @post.comments.create(comment_params.merge(user: current_user, post_id: @post.id))
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:title, :body, :post_id)
+  end
+end
